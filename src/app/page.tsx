3294bcc2e35c9aea3b18f009291d1506637378b1
QuangAnhTo <quangato072@gmail.com>
2024-12-31 23:25:24 +0700

    layout refactoring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,6 @@ export default function Performances() {
       <h1 className="text-2xl font-bold text-green-400">
         Navigations
       </h1>
-      <Link href="projects" className="font-bold text-orange-600 hover:text-pink-400">Projects</Link>
-      <Link href="tasks" className="font-bold text-orange-600 hover:text-pink-400">Tasks</Link>
-      <Link href="workLogs" className="font-bold text-orange-600 hover:text-pink-400">Work Logs</Link>
-      <Link href="performances" className="font-bold text-orange-600 hover:text-pink-400">Performances</Link>
-      <Link href="settings" className="font-bold text-orange-600 hover:text-pink-400">Settings</Link>
-      <Link href="login" className="font-bold text-orange-600 hover:text-pink-400">Log in</Link>
     </div>
   )
 }
