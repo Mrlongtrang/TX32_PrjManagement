e9447f2a1e286723fbe50b0788995f58e0401993
QuangAnhTo <quangato072@gmail.com>
2025-01-01 16:01:12 +0700

    refactor: create new top bar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,19 @@
 import Navbar from "@/components/Navbar/Navbar";
-import Link from "next/link";
+import Topbar from "@/components/Navbar/Topbar";
 import React from "react";
 import "tailwindcss/tailwind.css";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        <div className="w-full">
-          <header className="flex flex-row justify-between p-4 bg-gray-800">
-            <h1 className="text-2xl font-bold text-white">
-              Project Management
-            </h1>
-            <div className="flex flex-row gap-4">
-              <Link
-                href="login"
-                className="font-bold text-white hover:text-green-400">
-                Log in
-              </Link>
-              <Link
-                href="signup"
-                className="font-bold text-white hover:text-green-400">
-                Sign up
-              </Link>
-            </div>
-          </header>
-        </div>
-        <div className="flex flex-row justify-between gap-4 p-2 w-full">
-          <div className="flex flex-col justify-center w-1/6 p-5 border-2 border-gray-500 gap-4 rounded-md">
+      <body className="flex flex-col items-center justify-start w-screen h-screen">
+        <Topbar />
+        <div className="flex flex-row justify-between gap-2 p-2 w-full h-full">
+          <div className="flex flex-col justify-between w-1/6 p-3 border-2 border-gray-200 gap-4 rounded-md shadow-md">
             <Navbar />
           </div>
-          <main className="w-5/6 pl-10 border-2 border-gray-400 rounded-md">
-            {children}
+          <main className="w-5/6 pl-10 border-2 border-gray-200 bg-slate-100 rounded-md shadow-md">
+            <div className="bg-white">{children}</div>
           </main>
         </div>
       </body>
